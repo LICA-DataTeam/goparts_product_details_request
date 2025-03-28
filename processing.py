0113eacd2f02fc4acc65c4b746d77ff15d8dc798
John Paul L. Santos <santosjohnpaul2222@gmail.com>
2025-03-28 12:10:51 +0800

    added documentation and hot loop optimization
    added documentation. Optimized hot loop leading to a 1.5x speed up

--- processing.py
+++ processing.py
@@ -4,8 +4,16 @@ import re
 import strsimpy
 import requests
 import io
+from time import time
 
 def create_excel_template():
+    """
+    Creates an Excel template with columns part_number, product, category, brand (case-sensitive).
+
+    Returns:
+        - df (pd.DataFrame): The DataFrame representation of the template.
+        - excel_data (bytes): The Excel file in binary format.
+    """
     df = pd.DataFrame([["" for i in range(4)]for j in range(20)], columns=["part_number", "product", "category", "brand"])
     output = io.BytesIO()
     with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
@@ -16,6 +24,15 @@ def create_excel_template():
     return df, excel_data
 
 def convert_result_to_excel(df_result):
+    """
+    Converts the result DataFrame into an Excel file.
+
+    Args:
+        df_result (pd.DataFrame): The DataFrame containing the results.
+
+    Returns:
+        excel_data (bytes): The Excel file in binary format.
+    """
     output = io.BytesIO()
     with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
         df_result[["details", "match1", "match2", "match1_cost", "match1_tier_1", "match2_cost", "match2_tier_1"]].to_excel(writer, sheet_name="results", index=False)
@@ -28,6 +45,15 @@ def convert_result_to_excel(df_result):
     return excel_data
 
 def request_redash_goparts_product_query(api_call):
+    """
+    Fetches product data from a Redash API call and formats it into a DataFrame.
+
+    Args:
+        api_call (str): The API endpoint URL.
+
+    Returns:
+        df (pd.DataFrame): The DataFrame containing the fetched product data.
+    """
     response = requests.get(str(api_call)).json()
     cols = ["part_number", "product", "category", "brand", "cost", "tier_1", "p_id", "pc_id", "ib_id"]
     df = pd.DataFrame(response["query_result"]["data"]["rows"])[cols]
@@ -35,12 +61,31 @@ def request_redash_goparts_product_query(api_call):
     return df
 
 def clean_str(text):
+    """
+    Cleans a string by removing non-alphanumeric characters and normalizing case.
+
+    Args:
+        text (str): The input string.
+
+    Returns:
+        cleaned_text (str): The cleaned string, or None if the input is NaN.
+    """
     if pd.isna(text):
         return
+    cleaned_text = re.sub(r"[^a-zA-Z0-9]", "", text).replace("ñ", "n").replace("Ñ", "N").lower()
 
-    return re.sub(r"[^a-zA-Z0-9]", "", text).replace("ñ", "n").replace("Ñ", "N").lower()
+    return cleaned_text
 
 def setup_df_needle(df1):
+    """
+    Prepares the input DataFrame by cleaning and adding new columns for matching.
+
+    Args:
+        df1 (pd.DataFrame): The input DataFrame.
+
+    Returns:
+        df1 (pd.DataFrame): The modified DataFrame with cleaned columns.
+    """
     df1["part_number_clean"] = df1["part_number"].map(clean_str)
     df1["product_clean"] = df1["product"].map(clean_str)
     df1["category_clean"] = df1["category"].map(clean_str)
@@ -49,6 +94,17 @@ def setup_df_needle(df1):
     return df1
 
 def setup_df_haystack(api_call):
+    """
+    Prepares the haystack DataFrame by fetching, cleaning, and structuring data.
+
+    Args:
+        api_call (str): The API endpoint URL.
+
+    Returns:
+        df2 (pd.DataFrame): The part_number and product DataFrame.
+        df2_category (pd.DataFrame): The category DataFrame.
+        df2_brand (pd.DataFrame): The brand DataFrame.
+    """
     df2 = request_redash_goparts_product_query(api_call)
     df2_category =  df2[["pc_id", "category"]].drop_duplicates()
     df2_brand = df2[["ib_id", "brand"]].drop_duplicates()
@@ -63,7 +119,18 @@ def setup_df_haystack(api_call):
 
     return df2, df2_category, df2_brand
 
-def jaccard(str1, str2): # complex, better for filtering out accidental matches
+def jaccard(str1, str2):
+    """
+    Computes the Jaccard similarity score between two strings.
+    similarity score is between 0 and 1.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        score (float): The Jaccard similarity score, or None if either string is NaN.
+    """
     if pd.isna(str1) or pd.isna(str2):
         return
     score = strsimpy.Jaccard(3).similarity(str1, str2)
@@ -71,6 +138,16 @@ def jaccard(str1, str2): # complex, better for filtering out accidental matches
     return score
 
 def row_average(row2):
+    """
+    Computes a weighted average score for a row based on matching scores.
+    ignores blank product details.
+
+    Args:
+        row2 (pd.DataFrame Row): The row containing matching scores.
+
+    Returns:
+        weighted_average (float): The weighted average similarity score.
+    """
     sum_row = 0
     n = 0
 
@@ -104,6 +181,15 @@ def row_average(row2):
     return weighted_average
 
 def details_concat(row1):
+    """
+    Concatenates product details from a row into a single string.
+
+    Args:
+        row1 (pd.DataFrame Row): The row containing details.
+
+    Returns:
+        details_concat_str (str): The concatenated details string.
+    """
     part_number = row1["part_number"]
     product = row1["product"]
     category = row1["category"]
@@ -125,6 +211,16 @@ def details_concat(row1):
     return details_concat_str
 
 def match_concat(row1, row2):
+    """
+    Concatenates matched details in row2 if it is given in row1.
+
+    Args:
+        row1 (pd.DataFramae Row): The row of the needle DataFrame.
+        row2 (pd.DataFramae Row): The row of the matched DataFrame.
+
+    Returns:
+        (str): The concatenated matching details string.
+    """
     match_concat_str = "|"
     if not pd.isna(row1["part_number"]):
         match_concat_str += row2["part_number"] + "|"
@@ -141,30 +237,54 @@ def match_concat(row1, row2):
     return match_concat_str
 
 def match_string(row1, df2, df2_category, df2_brand):
-        df2["part_number_score"] = df2["part_number_clean"].map(lambda row2: jaccard(row1["part_number_clean"], row2))
-        df2["product_score"] = df2["product_clean"].map(lambda row2: jaccard(row1["product_clean"], row2))
-        df2_category["category_score"] = df2_category["category_clean"].map(lambda row2: jaccard(row1["category_clean"], row2))
-        df2_brand["brand_score"] = df2_brand["brand_clean"].map(lambda row2: jaccard(row1["brand_clean"], row2))
+    """
+    Matches a single row against the haystack DataFrame and computes scores.
 
-        df_result = df2.merge(df2_category, on="pc_id", how="left").merge(df2_brand, on="ib_id", how="left")
+    Args:
+        row1 (pd.Series): The row to match.
+        df2 (pd.DataFrame): The main haystack DataFrame.
+        df2_category (pd.DataFrame): The category DataFrame.
+        df2_brand (pd.DataFrame): The brand DataFrame.
 
-        df2.drop(columns=["part_number_score", "product_score"], inplace=True)
-        df2_category.drop(columns=["category_score"], inplace=True)
-        df2_brand.drop(columns=["brand_score"], inplace=True)
+    Returns:
+        tuple: A tuple containing match details, scores, and other metrics.
+    """
+    df2["part_number_score"] = df2["part_number_clean"].map(lambda row2: jaccard(row1["part_number_clean"], row2))
+    df2["product_score"] = df2["product_clean"].map(lambda row2: jaccard(row1["product_clean"], row2))
+    df2_category["category_score"] = df2_category["category_clean"].map(lambda row2: jaccard(row1["category_clean"], row2))
+    df2_brand["brand_score"] = df2_brand["brand_clean"].map(lambda row2: jaccard(row1["brand_clean"], row2))
 
-        df_result["average_score"] = df_result.apply(row_average, axis=1)
-        df_result["match_concat"] = df_result.apply(lambda row2: match_concat(row1, row2), axis=1)
-        df_result.sort_values(by=["average_score", "tier_1"], ascending=[False, True], inplace=True)
-        df_first2 = df_result.head(2)[["match_concat", "p_id", "average_score", "cost", "tier_1"]]
-        str_high2, id_high2, score2, cost2, tier1_2 = df_first2.iloc[0]
-        str_high3, id_high3, score3, cost3, tier1_3 = df_first2.iloc[1]
-        delta = score2 - score3
+    df_result = df2.merge(df2_category, on="pc_id", how="left").merge(df2_brand, on="ib_id", how="left")
 
-        return str_high2, str_high3, cost2, tier1_2, cost3, tier1_3, id_high2, id_high3, score2, score3, delta
+    df2.drop(columns=["part_number_score", "product_score"], inplace=True)
+    df2_category.drop(columns=["category_score"], inplace=True)
+    df2_brand.drop(columns=["brand_score"], inplace=True)
+
+    df_result["average_score"] = df_result.apply(row_average, axis=1)
+    df_result.sort_values(by=["average_score", "tier_1"], ascending=[False, True], inplace=True)
+    df_first2 = df_result.head(2)
+    df_first2["match_concat"] = df_first2.apply(lambda row2: match_concat(row1, row2), axis=1)
+    df_first2 = df_first2[["match_concat", "p_id", "average_score", "cost", "tier_1"]]
+    str_high2, id_high2, score2, cost2, tier1_2 = df_first2.iloc[0]
+    str_high3, id_high3, score3, cost3, tier1_3 = df_first2.iloc[1]
+    delta = score2 - score3
+
+    return str_high2, str_high3, cost2, tier1_2, cost3, tier1_3, id_high2, id_high3, score2, score3, delta
 
 def match_strings(df1, api_call):
+    """
+    Matches all rows in the input DataFrame against the haystack and computes results.
+
+    Args:
+        df1 (pd.DataFrame): The input DataFrame.
+        api_call (str): The API endpoint URL.
+
+    Returns:
+        pd.DataFrame: The DataFrame containing match results and metrics.
+    """
     df1 = setup_df_needle(df1)
     df2, df2_category, df2_brand = setup_df_haystack(api_call)
+    ti = time()
     results = df1.apply(lambda row1: match_string(row1, df2, df2_category, df2_brand), axis=1)
     df1[["match1", "match2", "match1_cost", "match1_tier_1", "match2_cost", "match2_tier_1", "id1", "id2", "score1", "score2", "delta_score"]] = pd.DataFrame(results.to_list(), index=results.index)
 
@@ -175,5 +295,6 @@ def match_strings(df1, api_call):
 
     df1["details"] = df1.apply(details_concat, axis=1)
     df1 = df1[["details", "match1", "match2", "match1_cost", "match1_tier_1", "match2_cost", "match2_tier_1", "id1", "id2", "score1", "score2", "delta_score", "relative_error" ]]
-
+    tf = time()
+    print("total time:", tf-ti)
     return df1
